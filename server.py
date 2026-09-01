24548b69585d458cdb014c535cae967cb170fae1
Franklin Zaldaña <fjz.ubq@gmail.com>
2026-09-01 10:49:51 -0300

    Fix: cron buenos-dias a las 8am UTC-11 + ventana 30min para alarmas

--- server.py
+++ server.py
@@ -314,8 +314,8 @@ def ejecutar_ciclo_uba():
         ahora = ahora_argentina()
         hoy_str = ahora.strftime("%Y-%m-%d")
 
-        # Buenos días (entre 8:00 y 8:09 ARG, una vez por día)
-        if ahora.hour == 8 and ahora.minute < 10 and not buenos_dias_ya_enviado_hoy(BUENOS_DIAS_STATE_UBA):
+        # Buenos días (entre 8:00 y 8:29 ARG, una vez por día)
+        if ahora.hour == 8 and ahora.minute < 30 and not buenos_dias_ya_enviado_hoy(BUENOS_DIAS_STATE_UBA):
             msg = f"🌅 *¡BUENOS DÍAS!*\n\n{leer_pendientes_dia()}"
             for chat in chats:
                 enviar_telegram_retry(send_url_uba, {"chat_id": chat, "text": msg})
@@ -449,8 +449,8 @@ def ejecutar_ciclo_vero():
         ahora = ahora_argentina()
         hoy_str = ahora.strftime("%Y-%m-%d")
 
-        # Buenos días (entre 8:00 y 8:09 ARG, una vez por día)
-        if ahora.hour == 8 and ahora.minute < 10 and not buenos_dias_ya_enviado_hoy(BUENOS_DIAS_STATE_VERO):
+        # Buenos días (entre 8:00 y 8:29 ARG, una vez por día)
+        if ahora.hour == 8 and ahora.minute < 30 and not buenos_dias_ya_enviado_hoy(BUENOS_DIAS_STATE_VERO):
             try:
                 eventos = gc.listar_eventos_dia(hoy_str)
             except Exception:
